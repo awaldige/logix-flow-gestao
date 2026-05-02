152b63d1428fc6ea2b65c6e07bfc74207b8020ba
awaldige <106982416+awaldige@users.noreply.github.com>
2026-05-01 22:20:16 -0300

    Update layout.tsx

--- logix-web/src/app/layout.tsx
+++ logix-web/src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Create Next App",
+  title: "LogixFlow - Gestão de Frota",
   description: "Sistema profissional de gestão de frota",
 };
 
